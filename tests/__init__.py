9731397f1d0362dbdba4ca8bf1a6414b6fa49a6f
Holedozer1229 <holedozer@icloud.com>
2025-05-25 12:16:58 -0500

    Create __init__.py

---
+++ tests/__init__.py
@@ -0,0 +1,10 @@
+__all__ = [
+    'test_scalar_field',
+    'test_entanglement',
+    'test_harmonics',
+    'test_ctc',
+    'test_mabk',
+    'test_ctc_tuning',
+    'test_j6_validation',
+    'test_geometry'
+]
